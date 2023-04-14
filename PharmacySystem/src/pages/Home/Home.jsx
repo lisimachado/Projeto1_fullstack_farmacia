5e4eb493be18d1199790dd4a579f6454f1a5e545
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-04-14 13:06:00 -0300

    Fix: correção de estrutura das rotas

--- PharmacySystem/src/pages/Home/Home.jsx
+++ PharmacySystem/src/pages/Home/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 
-const Home = () => {
+export const Home = () => {
   return (
-    <main className="home__container">
+    <div className="home__container">
 			<h1 className="home__title">Página de login/home Pharmacy System</h1>
-    </main>
+    </div>
   );
 };
-
-export default Home;
