5958d0d8ebc10103c452c3e70ca3a22cbe01dc8e
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-04-15 00:16:09 -0300

    Style: alteração estilo no header

--- PharmacySystem/src/components/Header/Header.jsx
+++ PharmacySystem/src/components/Header/Header.jsx
@@ -11,21 +11,20 @@ export const Header = () => {
   }
 	
   return (
-    <Navbar bg="grey" variant="grey">
-      <Navbar.Brand href="/">
-        <img
-          src={logo} 
-          height="70"
-          className="d-inline-block align-top"
-          alt="Logo"
-        />
-      </Navbar.Brand>
-      <Nav Nav className="justify-content-end">
-        <Nav.Link href="/farmacias">Farmácias</Nav.Link>
-        <Nav.Link href="/medicamentos">Medicamentos</Nav.Link>
-				<Nav.Link href="/cadastro">Cadastro</Nav.Link>
-       
-      </Nav>
+    <Navbar variant="grey">
+				<Navbar.Brand style={{ marginLeft: '5%'}} href="/">
+					<img
+						src={logo} 
+						height="70"
+						className="d-inline-block align-top"
+						alt="Logo"
+					/>
+				</Navbar.Brand>
+				<Nav className="ms-auto nav-with-margin" style={{ marginRight: '5%', fontWeight: 'bold'}}>
+					<Nav.Link href="/farmacias">Farmácias</Nav.Link>
+					<Nav.Link href="/medicamentos">Medicamentos</Nav.Link>
+					<Nav.Link href="/cadastro">Cadastro</Nav.Link>
+				</Nav>
     </Navbar>
   );
 }
