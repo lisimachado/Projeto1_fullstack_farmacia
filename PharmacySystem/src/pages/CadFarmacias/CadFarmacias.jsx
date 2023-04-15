b309867d1fc9d72e900e1e79a233d2053e3839d8
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-04-15 13:55:36 -0300

    Feat: Novos componentes e pages

--- PharmacySystem/src/pages/CadFarmacias/CadFarmacias.jsx
+++ PharmacySystem/src/pages/CadFarmacias/CadFarmacias.jsx
@@ -1,10 +1,10 @@
-import { Formshop } from "../../components/Formshop/Formshop";
+//import { Formshop } from "../../components/Formshop/Formshop";
 
 export const CadFarmacias = () => {
   return (
     <div className="container">
 			<h3 className="title">Cadastrar nova loja/farmácia</h3>
-			<Formshop/>
+			
 			
     </div>
   );
