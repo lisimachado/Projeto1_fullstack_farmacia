5bdfc94b9614d01da0e1dde4bf61d239c4f56e8c
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-04-16 22:26:38 -0300

    Feat: Adicionando funcionalidade de mapa na pagina de lojas

---
+++ PharmacySystem/src/components/Mapa/Mapa.jsx
@@ -0,0 +1,105 @@
+import React, { useRef, useEffect } from 'react';
+import L from 'leaflet';
+import { renderToString } from 'react-dom/server';
+
+import 'leaflet/dist/leaflet.css';
+import './Mapa.css';
+
+export const Mapa = () => {
+  const mapRef = useRef(null);
+
+  useEffect(() => {
+    // Renderiza o mapa na posição e com o zoom definidos
+    const map = L.map(mapRef.current).setView([-27.5998621, -48.5483227], 11);
+
+    // Adiciona o tile layer do OpenStreetMap
+    L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors',
+      maxZoom: 18,
+    }).addTo(map);
+
+    // Adiciona marcadores para as lojas
+    const lojas = [
+      { cod: 'Loja 001',
+				cnpj: '42.589.746/0001-00', 
+				razaoSocial: 'LABPharmacy Comércio de Produtos Farmacêuticos S.A.',
+				fantasia: 'PharmaLife',
+				email: 'contatoloja01@pharmalife.com.br',
+				telefone: '(48)8022-4000',
+				endereco: 'Rua: Presidente Coutinho, 100, bairro Centro, Florianópolis/SC CEP 88.050-000',
+				latlng: [-27.5906434, -48.5506324] },
+
+				{
+					cod: 'Loja 002',
+					cnpj: '42.589.746/0002-38',
+					razaoSocial: 'LABPharmacy Comércio de Produtos Farmacêuticos S.A.',
+					fantasia: 'PharmaLife',
+					email: 'contatoloja02@pharmalife.com.br',
+					telefone: '(48)8022-4001',
+					endereco: 'Rua: Almirante Lamego, 210, bairro Centro, Florianópolis/SC CEP 88.050-010',
+					latlng: [-27.5866780, -48.4989027],
+				},
+
+				{
+					cod: 'Loja 003',
+					cnpj: '42.589.746/0003-43',
+					razaoSocial: 'LABPharmacy Comércio de Produtos Farmacêuticos S.A.',
+					fantasia: 'PharmaLife',
+					email: 'contatoloja03@pharmalife.com.br',
+					telefone: '(48)8022-4002',
+					endereco: 'Rua: Tenente Silveira, 111, bairro Centro, Florianópolis/SC CEP 88.015-300',
+					latlng: [-27.6620, -48.5013],
+				},
+
+				{
+					cod: 'Loja 004',
+					cnpj: '42.589.746/0004-14',
+					razaoSocial: 'LABPharmacy Comércio de Produtos Farmacêuticos S.A.',
+					fantasia: 'PharmaLife',
+					email: 'contatoloja04@pharmalife.com.br',
+					telefone: '(48)8022-4004',
+					endereco: 'Rua: Deodoro, 147, bairro Centro, Florianópolis/SC CEP 88.015-902',
+					latlng: [-27.5887649, -48.5224589],
+				},
+
+				{
+					cod: 'Loja 005',
+					cnpj: '42.589.746/0005-62',
+					razaoSocial: 'LABPharmacy Comércio de Produtos Farmacêuticos S.A.',
+					fantasia: 'PharmaLife',
+					email: 'contatoloja05@pharmalife.com.br',
+					telefone: '(48)8022-4005',
+					endereco: 'Rua: Deodoro, 147, bairro Centro, Florianópolis/SC CEP 88.015-902',
+					latlng: [-27.5851554, -48.5784047],
+				},
+
+				{
+					cod: 'Loja 006',
+					cnpj: '42.589.746/0006-11',
+					razaoSocial: 'LABPharmacy Comércio de Produtos Farmacêuticos S.A.',
+					fantasia: 'PharmaLife',
+					email: 'contatoloja06@pharmalife.com.br',
+					telefone: '(48)8022-4006',
+					endereco: 'Rua: Deodoro, 147, bairro Centro, Florianópolis/SC CEP 88.015-902',
+					latlng: [-27.6122212, -48.5899312],
+				},
+    ];
+
+    lojas.forEach((loja) => {
+      const popupContent = (
+        <div>
+          <h5>{loja.fantasia}</h5>
+          <p>Razão social: {loja.razaoSocial}</p>
+          <p>CNPJ: {loja.cnpj}</p>
+          <p>Email: {loja.email}</p>
+          <p>Telefone: {loja.telefone}</p>
+          <p>Endereço: {loja.endereco}</p>
+        </div>
+      );
+      const popupHTML = renderToString(popupContent);
+      L.marker(loja.latlng).addTo(map).bindPopup(popupHTML);
+    });
+  }, []);
+
+  return <div style={{ height: '300px' }} ref={mapRef}></div>;
+};
