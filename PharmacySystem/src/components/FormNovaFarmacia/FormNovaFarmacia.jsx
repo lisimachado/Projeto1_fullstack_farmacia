e85a5abef74990f3c14ecc5e3af61fbe1d79fc38
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-04-18 13:11:11 -0300

    Feat: funcionalidade de localStorage na pagina de cadastro farmacia

--- PharmacySystem/src/components/FormNovaFarmacia/FormNovaFarmacia.jsx
+++ PharmacySystem/src/components/FormNovaFarmacia/FormNovaFarmacia.jsx
@@ -1,46 +1,39 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './FormNovaFarmacia.css';
 import axios from 'axios';
 
 export const FormNovaFarmacia = ()=> {
 
-	//Função para o campo CNPJ fique no formato correto
-	// const [codLoja, setCodLoja] = useState('');
 	const [cnpj, setCNPJ] = useState('');
-	// const [razaoSocial, setRazaoSocial] = useState('');
-	// const [fantasia, setFantasia] = useState('');
-	// const [email, setEmail] = useState('');
-	// const [telefone, setTelefone] = useState('');
 	const [cep, setCep] = useState('');
-	// const [logradouro, setLogradouro] = useState('');
-	// const [numero, setNumero] = useState('');
-	// const [complemento, setComplemento] = useState('');
-	// const [bairro, setBairro] = useState('');
-	// const [cidade, setCidade] = useState('');
-	// const [uf, setUf] = useState('');
-	// const [pais, setPais] = useState('');
-	// const [formFarmaciaValue, setFormFarmaciaValue] = useState('');
-
+	
 	//Salvar no localStorage
 
-	const [formValues, setFormValues] = useState({});
-	const handleChange = (event) => {
-  setFormValues({ ...formValues, [event.target.name]: event.target.value });
-	console.log(formValues)
-	};
+	const [razaoSocial, setRazaoSocial] = useState("");
+  const [codigo, setCodigo] = useState("");
 
-	const [Key, setKey] = useState(Math.floor(Math.random() * 10000) + 1);
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+    localStorage.setItem("razaoSocial", razaoSocial);
+    localStorage.setItem("codigo", codigo);
 
 
-	function handleSubmit(e){
-		e.preventDefault()
-		localStorage.setItem(Key, JSON.stringify(formValues));
-		setKey(Math.floor(Math.random() * 10000) + 1);
-	}
-	
-	
+		
+    alert("Dados salvos com sucesso!");
+		document.getElementsByClassName('FormCadastro').reset();
+  };
+
+  const handleRazaoSocialChange = (event) => {
+    setRazaoSocial(event.target.value);
+  };
+
+  const handleCodigoChange = (event) => {
+    setCodigo(event.target.value);
+  };
+
+
 
 	//Máscara CNPJ
 	function handleCNPJChange(event) {
@@ -68,18 +61,19 @@ export const FormNovaFarmacia = ()=> {
   };
 
 	
-
-
 	//Formulário
 
   return (		
 		<container className="container">
-			<Form className="FormCadastro">
+			<Form className="FormCadastro"
+			onSubmit={handleFormSubmit}>
 				<div className="form-group row">
 					<Form.Group className="col-9" id="form_nome">
 						<Form.Label>Razão Social</Form.Label>
-						<Form.Control type="text" 
-						onChange={handleChange} />
+						<Form.Control 
+						type="text" 
+						value={razaoSocial} 
+						onChange={handleRazaoSocialChange} />
 					</Form.Group>
 
 					<Form.Group className="col-3" id="form_codigo">
@@ -87,7 +81,8 @@ export const FormNovaFarmacia = ()=> {
 						<Form.Control 
 						type="number" 
 						inputMode="numeric" 
-						onChange={handleChange}/>
+						value={codigo} 
+						onChange={handleCodigoChange}/>
 					</Form.Group>
 				</div>
 
@@ -95,7 +90,7 @@ export const FormNovaFarmacia = ()=> {
 					<Form.Group className="col-8" id="form_fantasia">
 						<Form.Label>Nome Fantasia</Form.Label>
 						<Form.Control type="textarea" 
-						onChange={handleChange}/>
+						/>
 					</Form.Group>
 
 					<Form.Group className="col-4" id="form_cnpj">
@@ -114,13 +109,13 @@ export const FormNovaFarmacia = ()=> {
 					<Form.Group className="col-8" id="form_email">
 						<Form.Label>E-mail</Form.Label>
 						<Form.Control type="email" 
-						onChange={handleChange}/>
+						/>
 					</Form.Group>
 
 					<Form.Group className="col-4" id="form_telefone">
 						<Form.Label>Telefone Celular</Form.Label>
 						<Form.Control type="tel" 
-						onChange={handleChange}/>
+						/>
 					</Form.Group>
 				</div>
 
@@ -144,7 +139,7 @@ export const FormNovaFarmacia = ()=> {
 						<Form.Control 
 						type="text"
 						value={endereco?.logradouro} readOnly 
-						onChange={handleChange}/>
+						/>
 					</Form.Group>
 				</div>
 
@@ -154,13 +149,13 @@ export const FormNovaFarmacia = ()=> {
 						<Form.Label>Número</Form.Label>
 						<Form.Control type="number" 
 						inputMode="numeric"
-						onChange={handleChange}/>
+						/>
 					</Form.Group>
 
 					<Form.Group className="col-4" id="form_complemento">
 						<Form.Label>Complemento</Form.Label>
 						<Form.Control type="text" 
-						onChange={handleChange}/>
+						/>
 					</Form.Group>
 
 					<Form.Group className="col-5" id="form_bairro">
@@ -168,7 +163,7 @@ export const FormNovaFarmacia = ()=> {
 						<Form.Control 
 						type="text"
 						value={endereco?.bairro} readOnly 
-						onChange={handleChange}/>
+						/>
 					</Form.Group>
 				</div>
 				
@@ -178,7 +173,7 @@ export const FormNovaFarmacia = ()=> {
 						<Form.Control 
 						type="text"
 						value={endereco?.localidade} readOnly
-						onChange={handleChange} />
+						 />
 					</Form.Group>
 
 					<Form.Group className="col-2" id="form_uf">
@@ -186,21 +181,21 @@ export const FormNovaFarmacia = ()=> {
 						<Form.Control 
 						type="text"
 						value={endereco?.uf} readOnly
-						onChange={handleChange}
+						
 						/>
 					</Form.Group>
 
 					<Form.Group className="col-4" id="form_pais">
 						<Form.Label>País</Form.Label>
 						<Form.Control type="text"
-						onChange={handleChange}/>
+						/>
 					</Form.Group>
 				</div>
 
 
-				<div className="d-flex justify-content-end">		
+				<div className="d-flex justify-content-end ">		
 					<Button 
-					className="btn-envio ms-auto" 
+					className="btn-envio ms-auto"
 					variant="success"
 					type="submit"
 					id="btn-cadastro">
