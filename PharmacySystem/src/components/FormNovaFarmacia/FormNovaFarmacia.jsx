ae63e2d197db3a5968fe434918719012a58cbd33
Lisiane Machado Martins <lisiannemachado@gmail.com>
2023-04-18 22:46:41 -0300

    Feat: Adicionando funções de localStorage na pagina de cadastro farmacias

--- PharmacySystem/src/components/FormNovaFarmacia/FormNovaFarmacia.jsx
+++ PharmacySystem/src/components/FormNovaFarmacia/FormNovaFarmacia.jsx
@@ -4,36 +4,59 @@ import React, { useState, useEffect } from 'react';
 import './FormNovaFarmacia.css';
 import axios from 'axios';
 
-export const FormNovaFarmacia = ()=> {
+export const FormNovaFarmacia = () => {
 
-	const [cnpj, setCNPJ] = useState('');
+	const [razaoSocial, setRazaoSocial] = useState('');
+	const [codLoja, setCodLoja] = useState('');
+	const [fantasia, setFantasia] = useState('');
+	const [CNPJ, setCNPJ] = useState('');
+	const [email, setEmail] = useState('');
+	const [telefone, setTelefone] = useState('');
 	const [cep, setCep] = useState('');
-	
+	const [logradouro, setLogradouro] = useState('');
+	const [numero, setNumero] = useState('');
+	const [complemento, setComplemento] = useState('');
+	const [bairro, setBairro] = useState('');
+	const [cidade, setCidade] = useState('');
+	const [uf, setUf] = useState('');
+	const [pais, setPais] = useState('');
+
+	const [formFarmaciaValue, setFormFarmaciaValue] = useState('');
+
 	//Salvar no localStorage
 
-	const [razaoSocial, setRazaoSocial] = useState("");
-  const [codigo, setCodigo] = useState("");
+	const handleFormSubmit = (event) => {
+		event.preventDefault();
 
-  const handleFormSubmit = (event) => {
-    event.preventDefault();
-    localStorage.setItem("razaoSocial", razaoSocial);
-    localStorage.setItem("codigo", codigo);
+		const novafarmacia = {
+			razaoSocial,
+			codLoja,
+			fantasia,
+			CNPJ,
+			email,
+			telefone,
+			cep,
+			logradouro,
+			numero,
+			complemento,
+			bairro,
+			cidade,
+			uf,
+			pais,
+		}
 
+		try {
+			const formularioAtualizado = [...formFarmaciaValue, novafarmacia]
+			setFormFarmaciaValue(formularioAtualizado)
 
-		
-    alert("Dados salvos com sucesso!");
-		document.getElementsByClassName('FormCadastro').reset();
-  };
+			localStorage.setItem("dadosFarmacia", JSON.stringify(formularioAtualizado))
 
-  const handleRazaoSocialChange = (event) => {
-    setRazaoSocial(event.target.value);
-  };
-
-  const handleCodigoChange = (event) => {
-    setCodigo(event.target.value);
-  };
-
-
+			console.log("Dados salvos com sucesso!")
+			alert("Dados salvos com sucesso!");
+		} catch (error) {
+			console.log(error)
+		}
+	};
 
 	//Máscara CNPJ
 	function handleCNPJChange(event) {
@@ -41,66 +64,86 @@ export const FormNovaFarmacia = ()=> {
 		setCNPJ(numCNPJ);
 	}
 
-	//Função para utilização da API de CEP
-	const [endereco, setEndereco] = useState();
 
-  const [formulario, setFormulario] = useState({
-    cep: "",
-  });
+	//FUNÇÃO DE BUSCAO CEP NA API
+	const buscarCep = () => {
+		fetch(`https://viacep.com.br/ws/${cep}/json/`)
+			.then((resposta) => resposta.json())
+			.then((dados) => preencherEndereco(dados));
+	};
 
-  const atualizarCampo = (campo, valor) => {
-    const novosDados = { ...formulario, [campo]: valor };
-    setFormulario(novosDados);
-		console.log(novosDados)
-  };
+	//FUNÇÃO DE PREENCHER OS DADOS DO FORMULÁRIO
+	const preencherEndereco = (dados) => {
+		setLogradouro(dados.logradouro)
+		setBairro(dados.bairro)
+		setCidade(dados.localidade)
+		setUf(dados.uf)
+	}
 
-  const buscarCep = () => {
-    fetch(`https://viacep.com.br/ws/${formulario.cep}/json/`)
-      .then((resposta) => resposta.json())
-      .then((dados) => setEndereco(dados));
-  };
+	//Limpar os campos dos inputs
+	const limparDados = () => {
+		setRazaoSocial("");
+		setCodLoja("");
+		setFantasia("");
+		setCNPJ("");
+		setEmail("");
+		setTelefone("");
+		setCep("");
+		setLogradouro("");
+		setNumero("");
+		setComplemento("");
+		setBairro("");
+		setCidade("");
+		setUf("");
+		setPais("");
+	}
 
-	
 	//Formulário
 
-  return (		
+	return (
 		<container className="container">
 			<Form className="FormCadastro"
-			onSubmit={handleFormSubmit}>
+				onSubmit={handleFormSubmit}>
 				<div className="form-group row">
 					<Form.Group className="col-9" id="form_nome">
 						<Form.Label>Razão Social</Form.Label>
-						<Form.Control 
-						type="text" 
-						value={razaoSocial} 
-						onChange={handleRazaoSocialChange} />
+						<Form.Control
+							type="text"
+							value={razaoSocial}
+							onChange={e => setRazaoSocial(e.target.value)} />
 					</Form.Group>
 
 					<Form.Group className="col-3" id="form_codigo">
 						<Form.Label>Código da Loja</Form.Label>
-						<Form.Control 
-						type="number" 
-						inputMode="numeric" 
-						value={codigo} 
-						onChange={handleCodigoChange}/>
+						<Form.Control
+							type="number"
+							inputMode="numeric"
+							value={codLoja}
+							onChange={e => setCodLoja(e.target.value)} />
 					</Form.Group>
 				</div>
 
 				<div className="form-group row">
 					<Form.Group className="col-8" id="form_fantasia">
 						<Form.Label>Nome Fantasia</Form.Label>
-						<Form.Control type="textarea" 
-						/>
+						<Form.Control type="textarea"
+							value={fantasia}
+							onChange={e => setFantasia(e.target.value)} />
 					</Form.Group>
 
-					<Form.Group className="col-4" id="form_cnpj">
+					<Form.Group className="col-4" id="form_CNPJ">
 						<Form.Label>CNPJ</Form.Label>
-						<Form.Control 
-							type="text"
-							maxLength={18}
-							pattern="\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}"
-							value={cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5')}
-							onChange={handleCNPJChange} 
+						<Form.Control
+							type="number"
+							inputMode="numeric"
+							maxNumber={14}
+							value={CNPJ}
+							onChange={e => setCNPJ(e.target.value)}
+
+						// pattern="\d{2}\.\d{3}\.\d{3}\/\d{4}-\d{2}"
+						// value={cnpj.replace(/(\d{2})(\d{3})(\d{3})(\d{4})(\d{2})/, '$1.$2.$3/$4-$5')}
+						// onChange={handleCNPJChange} 
+
 						/>
 					</Form.Group>
 				</div>
@@ -108,13 +151,17 @@ export const FormNovaFarmacia = ()=> {
 				<div className="form-group row">
 					<Form.Group className="col-8" id="form_email">
 						<Form.Label>E-mail</Form.Label>
-						<Form.Control type="email" 
+						<Form.Control type="email"
+							value={email}
+							onChange={e => setEmail(e.target.value)}
 						/>
 					</Form.Group>
 
 					<Form.Group className="col-4" id="form_telefone">
 						<Form.Label>Telefone Celular</Form.Label>
-						<Form.Control type="tel" 
+						<Form.Control type="tel"
+							value={telefone}
+							onChange={e => setTelefone(e.target.value)}
 						/>
 					</Form.Group>
 				</div>
@@ -122,23 +169,26 @@ export const FormNovaFarmacia = ()=> {
 				<div className="row">
 					<Form.Group className="col-2" id="form_cep">
 						<Form.Label>CEP</Form.Label>
-						<Form.Control 
-						type="text"
-						value={formulario.cep}
-						onChange={(evento) => atualizarCampo("cep", evento.target.value)} />
+						<Form.Control
+							type="text"
+							value={cep}
+							onChange={(e) => setCep(e.target.value)} />
+
 					</Form.Group>
 
 					<Form.Group className="col-2">
-						<Button id="btnCep" 
-						onClick={buscarCep} 
-						variant="secondary">Buscar CEP</Button>
+						<Button id="btnCep"
+							onClick={buscarCep}
+							variant="secondary">Buscar CEP</Button>
 					</Form.Group>
 
 					<Form.Group className="col-8" id="form_logradouro">
 						<Form.Label>Logradouro</Form.Label>
-						<Form.Control 
-						type="text"
-						value={endereco?.logradouro} readOnly 
+						<Form.Control
+							type="text"
+							// value={endereco?.logradouro} readOnly 
+							value={logradouro}
+							onChange={e => setLogradouro(e.target.value)}
 						/>
 					</Form.Group>
 				</div>
@@ -147,58 +197,78 @@ export const FormNovaFarmacia = ()=> {
 				<div className="form-group row">
 					<Form.Group className="col-3" id="form_numero">
 						<Form.Label>Número</Form.Label>
-						<Form.Control type="number" 
-						inputMode="numeric"
+						<Form.Control type="number"
+							inputMode="numeric"
+							value={numero}
+							onChange={e => setNumero(e.target.value)}
 						/>
 					</Form.Group>
 
 					<Form.Group className="col-4" id="form_complemento">
 						<Form.Label>Complemento</Form.Label>
-						<Form.Control type="text" 
+						<Form.Control type="text"
+							value={complemento}
+							onChange={e => setComplemento(e.target.value)}
 						/>
 					</Form.Group>
 
 					<Form.Group className="col-5" id="form_bairro">
 						<Form.Label>Bairro</Form.Label>
-						<Form.Control 
-						type="text"
-						value={endereco?.bairro} readOnly 
+						<Form.Control
+							type="text"
+							// value={endereco?.bairro} readOnly 
+							value={bairro}
+							onChange={e => setBairro(e.target.value)}
 						/>
 					</Form.Group>
 				</div>
-				
+
 				<div className="form-group row">
 					<Form.Group className="col-6" id="form_cidade">
 						<Form.Label>Cidade</Form.Label>
-						<Form.Control 
-						type="text"
-						value={endereco?.localidade} readOnly
-						 />
+						<Form.Control
+							type="text"
+							// value={endereco?.localidade} readOnly
+							value={cidade}
+							onChange={e => setCidade(e.target.value)}
+						/>
 					</Form.Group>
 
 					<Form.Group className="col-2" id="form_uf">
 						<Form.Label>Estado/UF</Form.Label>
-						<Form.Control 
-						type="text"
-						value={endereco?.uf} readOnly
-						
+						<Form.Control
+							type="text"
+							// value={endereco?.uf} readOnly
+							value={uf}
+							onChange={e => setUf(e.target.value)}
+
 						/>
 					</Form.Group>
 
 					<Form.Group className="col-4" id="form_pais">
 						<Form.Label>País</Form.Label>
 						<Form.Control type="text"
+							value={pais}
+							onChange={e => setPais(e.target.value)}
 						/>
 					</Form.Group>
 				</div>
 
+				<div className="d-flex justify-content-end ">
+					<Button
+						className="btn-envio ms-auto"
+						variant="secondary"
+						type="submit"
+						id="btn-cadastro"
+						onClick={limparDados}>  {/*Criar função para limpar os dados */}
+						Limpar Dados
+					</Button>
 
-				<div className="d-flex justify-content-end ">		
-					<Button 
-					className="btn-envio ms-auto"
-					variant="success"
-					type="submit"
-					id="btn-cadastro">
+					<Button
+						className="btn-envio ms-auto"
+						variant="success"
+						type="submit"
+						id="btn-cadastro">
 						Enviar Cadastro
 					</Button>
 				</div>
@@ -207,5 +277,5 @@ export const FormNovaFarmacia = ()=> {
 			{/* Ajuste temporário para o footer não ficar sobrepondo o conteudo da página. */}
 			<br /><br /><br />
 		</container>
-  );
+	);
 }
